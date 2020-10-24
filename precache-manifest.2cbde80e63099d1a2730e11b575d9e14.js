4a63fcf044f600cb7711d80f1529c4a892bcbaf1
AnthonySosa <tony96sosa@hotmail.com>
2020-10-24 13:52:29 -0300

    Updates

---
+++ precache-manifest.2cbde80e63099d1a2730e11b575d9e14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17f5e61f5ab6cee08e2f0339be33c61",
+    "url": "/TodoList-1-React/index.html"
+  },
+  {
+    "revision": "590e25b181f1212f8cd2",
+    "url": "/TodoList-1-React/static/css/main.cee5f82c.chunk.css"
+  },
+  {
+    "revision": "085f26cd9ea6abfe0d67",
+    "url": "/TodoList-1-React/static/js/2.80c7c8cb.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/TodoList-1-React/static/js/2.80c7c8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590e25b181f1212f8cd2",
+    "url": "/TodoList-1-React/static/js/main.8e771f63.chunk.js"
+  },
+  {
+    "revision": "75c2bed30a9d025a690a",
+    "url": "/TodoList-1-React/static/js/runtime-main.d6b7b5db.js"
+  }
+]);
